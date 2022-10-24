a04b6122852fddf40718aa9ec8106cf0750e632c
Gerrod Schirtzinger <gerrod.l.schirtzinger@gmail.com>
2022-10-24 11:42:27 -0400

    update pwa

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,37 @@
+// service-worker.js
+
 // set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
-    prefix: 'gerrod-schirtzinger',
-    suffix: 'v1',
-    precache: 'precache',
-    runtime: 'runtime-cache'
+  prefix: 'gerrod-schirtzinger',
+  suffix: 'v1.0',
+  precache: 'precache',
+  runtime: 'runtime-cache',
 });
 
 // let Service Worker take control of pages ASAP
-workbox.skipWaiting();
-workbox.clientsClaim();
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
 
 // let Workbox handle our precache list
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
-// use `networkFirst` strategy for `*.html`, like all my posts
+// use `NetworkFirst` strategy for html
+workbox.routing.registerRoute(/\.html$/, new workbox.strategies.NetworkFirst());
+
+// use `NetworkFirst` strategy for css and js
 workbox.routing.registerRoute(
-    /\.html$/,
-    workbox.strategies.networkFirst()
+  /\.(?:js|css)$/,
+  new workbox.strategies.NetworkFirst()
 );
 
-// use `cacheFirst` strategy for images
+// use `CacheFirst` strategy for images
 workbox.routing.registerRoute(
-    /assets\/(img|icons|images)/,
-    workbox.strategies.cacheFirst()
+  /assets\/(img|icons)/,
+  new workbox.strategies.CacheFirst()
 );
 
-// third party files
+// use `StaleWhileRevalidate` third party files
 workbox.routing.registerRoute(
-    /^https?:\/\/cdn.staticfile.org/,
-    workbox.strategies.staleWhileRevalidate()
+  /^https?:\/\/cdn.staticfile.org/,
+  new workbox.strategies.StaleWhileRevalidate()
 );
